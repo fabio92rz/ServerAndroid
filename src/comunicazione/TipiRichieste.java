c6f8c20aadfe6a0837e95ec28d447f77a0e0a7e7
fabio92rz <fabio92rz@gmail.com>
2016-07-09 18:22:54 +0200

    trying to implement server side

--- src/comunicazione/TipiRichieste.java
+++
@@ -1,7 +0,0 @@
-package comunicazione;
-
-public class TipiRichieste 
-{
-	public final static int Registrazione = 0;
-	public final static int Login = 1;
-}
--- src/comunicazione/TipiRisposte.java
+++
@@ -1,6 +0,0 @@
-package comunicazione;
-
-public class TipiRisposte {
-	public final static int Successo = 0;
-	public final static int Fallimento = 1;	
-}
